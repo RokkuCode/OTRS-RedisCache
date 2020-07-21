578ad474722baccc3b47f8f1916a24b3da48e2c7
pyloor <dev@strace.it>
2020-07-21 03:09:42 +0200

    add redis server socket

--- Kernel/System/Cache/Redis.pm
+++ Kernel/System/Cache/Redis.pm
@@ -1,5 +1,6 @@
 # --
 # Copyright (C) 2019 by Yuri Myasoedov <ymyasoedov@yandex.ru>
+# Copyright (C) 2020 by Holger Schramm <dev@strace.it>
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -13,27 +14,25 @@ use warnings;
 
 use Encode;
 
-our @ObjectDependencies = (
-    'Kernel::Config',
-    'Kernel::System::Log',
-    'Kernel::System::Main',
-    'Kernel::System::Storable',
-);
+our @ObjectDependencies = ( 'Kernel::Config', 'Kernel::System::Log', 'Kernel::System::Main', 'Kernel::System::Storable', );
 
 my $NamespaceKey = 'namespaces';
 
-sub new {
+
+sub new
+{
     my ( $Type, %Param ) = @_;
 
     my $Self = {};
     bless( $Self, $Type );
 
     # Store Redis config
-    my $ConfigObject   = $Kernel::OM->Get('Kernel::Config');
+    my $ConfigObject = $Kernel::OM->Get( 'Kernel::Config' );
     $Self->{Config} = {
-        Address        => $ConfigObject->Get('Cache::Redis')->{Server}         || '127.0.0.1:6379',
-        DatabaseNumber => $ConfigObject->Get('Cache::Redis')->{DatabaseNumber} || 0,
-        RedisFast      => $ConfigObject->Get('Cache::Redis')->{RedisFast}      || 0,
+        Address        => $ConfigObject->Get( 'Cache::Redis' )->{ServerTCP}      || '127.0.0.1:6379',
+        Socket         => $ConfigObject->Get( 'Cache::Redis' )->{ServerSocket}   || '/var/run/redis/redis-server.sock',
+        DatabaseNumber => $ConfigObject->Get( 'Cache::Redis' )->{DatabaseNumber} || 0,
+        RedisFast      => $ConfigObject->Get( 'Cache::Redis' )->{RedisFast}      || 0,
     };
 
     # Not connected yet
@@ -42,12 +41,16 @@ sub new {
     return $Self;
 }
 
-sub Set {
+
+sub Set
+{
     my ( $Self, %Param ) = @_;
 
-    for my $Needed (qw(Type Key Value TTL)) {
-        if ( !defined $Param{$Needed} ) {
-            $Kernel::OM->Get('Kernel::System::Log')->Log(
+    for my $Needed ( qw(Type Key Value TTL) )
+    {
+        if ( !defined $Param{$Needed} )
+        {
+            $Kernel::OM->Get( 'Kernel::System::Log' )->Log(
                 Priority => 'error',
                 Message  => "Need $Needed!"
             );
@@ -66,8 +69,8 @@ sub Set {
 
         # Wrap the data, because we don't know, what user expects
         # to get from cache: reference to SCALAR or common SCALAR
-        my $StorableObject = $Kernel::OM->Get('Kernel::System::Storable');
-        my $Data = $StorableObject->Serialize(
+        my $StorableObject = $Kernel::OM->Get( 'Kernel::System::Storable' );
+        my $Data           = $StorableObject->Serialize(
             Data => {
                 _Data => $Param{Value},
             },
@@ -76,8 +79,10 @@ sub Set {
         $Self->{Redis}->set( $Key, $Data, 'EX', $Param{TTL} );
 
     };
-    if ($@) {
-        $Kernel::OM->Get('Kernel::System::Log')->Log(
+
+    if ( $@ )
+    {
+        $Kernel::OM->Get( 'Kernel::System::Log' )->Log(
             Priority => 'error',
             Message  => $@,
         );
@@ -87,12 +92,16 @@ sub Set {
     return 1;
 }
 
-sub Get {
+
+sub Get
+{
     my ( $Self, %Param ) = @_;
 
-    for my $Needed (qw(Type Key)) {
-        if ( !defined $Param{$Needed} ) {
-            $Kernel::OM->Get('Kernel::System::Log')->Log(
+    for my $Needed ( qw(Type Key) )
+    {
+        if ( !defined $Param{$Needed} )
+        {
+            $Kernel::OM->Get( 'Kernel::System::Log' )->Log(
                 Priority => 'error',
                 Message  => "Need $Needed!"
             );
@@ -105,32 +114,36 @@ sub Get {
 
     my $Key = encode( 'UTF-8', $Param{Type} . ':' . $Param{Key} );
     my $Value;
-    eval {
-        $Value = $Self->{Redis}->get($Key);
-    };
-    if ($@) {
-        Kernel::OM->Get('Kernel::System::Log')->Log(
+
+    eval { $Value = $Self->{Redis}->get( $Key ); };
+
+    if ( $@ )
+    {
+        Kernel::OM->Get( 'Kernel::System::Log' )->Log(
             Priority => 'error',
             Message  => $@,
         );
         return;
     }
+
     return if !$Value;
 
-    my $StorableObject = $Kernel::OM->Get('Kernel::System::Storable');
-    my $Data = $StorableObject->Deserialize(
-        Data => $Value,
-    );
+    my $StorableObject = $Kernel::OM->Get( 'Kernel::System::Storable' );
+    my $Data           = $StorableObject->Deserialize( Data => $Value, );
 
     return $Data->{_Data};
 }
 
-sub Delete {
+
+sub Delete
+{
     my ( $Self, %Param ) = @_;
 
-    for my $Needed (qw(Type Key)) {
-        if ( !defined $Param{$Needed} ) {
-            $Kernel::OM->Get('Kernel::System::Log')->Log(
+    for my $Needed ( qw(Type Key) )
+    {
+        if ( !defined $Param{$Needed} )
+        {
+            $Kernel::OM->Get( 'Kernel::System::Log' )->Log(
                 Priority => 'error',
                 Message  => "Need $Needed!"
             );
@@ -144,11 +157,12 @@ sub Delete {
     my $Key = encode( 'UTF-8', $Param{Type} . ':' . $Param{Key} );
 
     my $Result;
-    eval {
-        $Result = $Self->{Redis}->del($Key);
-    };
-    if ($@) {
-        Kernel::OM->Get('Kernel::System::Log')->Log(
+
+    eval { $Result = $Self->{Redis}->del( $Key ); };
+
+    if ( $@ )
+    {
+        Kernel::OM->Get( 'Kernel::System::Log' )->Log(
             Priority => 'error',
             Message  => $@,
         );
@@ -158,7 +172,9 @@ sub Delete {
     return !!$Result;
 }
 
-sub CleanUp {
+
+sub CleanUp
+{
     my ( $Self, %Param ) = @_;
 
     # Redis removes expired data itself and we don't care about it
@@ -168,36 +184,47 @@ sub CleanUp {
     return if !$Self->{Redis} && !$Self->_Connect();
 
     eval {
-        if ( !$Param{Type} && !$Param{KeepTypes} ) {
+
+        if ( !$Param{Type} && !$Param{KeepTypes} )
+        {
             $Self->{Redis}->flushdb();
             return 1;
         }
 
         my @TypeList;
-        if ( $Param{Type} ) {
+        if ( $Param{Type} )
+        {
             push @TypeList, $Param{Type};
         }
-        else {
-            @TypeList = $Self->{Redis}->smembers($NamespaceKey);
+        else
+        {
+            @TypeList = $Self->{Redis}->smembers( $NamespaceKey );
         }
 
-        if ( $Param{KeepTypes} ) {
-            my $KeepTypesRegex = join( '|', map {"\Q$_\E"} @{ $Param{KeepTypes} } );
+        if ( $Param{KeepTypes} )
+        {
+            my $KeepTypesRegex =
+                join( '|', map { "\Q$_\E" } @{ $Param{KeepTypes} } );
             @TypeList = grep { $_ !~ m{/$KeepTypesRegex/?$}smx } @TypeList;
         }
 
         return 1 if !@TypeList;
 
-        for my $Type (@TypeList) {
+        for my $Type ( @TypeList )
+        {
             my ( $Cursor, $KeysRef ) = ( 0, [] );
-            do {
-                ( $Cursor, $KeysRef ) = $Self->{Redis}->scan( $Cursor, MATCH => "$Type:*" );
+            do
+            {
+                ( $Cursor, $KeysRef ) =
+                    $Self->{Redis}->scan( $Cursor, MATCH => "$Type:*" );
                 $Self->{Redis}->del( @{$KeysRef} ) if @{$KeysRef};
-            } while ($Cursor);
+            } while ( $Cursor );
         }
     };
-    if ($@) {
-        Kernel::OM->Get('Kernel::System::Log')->Log(
+
+    if ( $@ )
+    {
+        Kernel::OM->Get( 'Kernel::System::Log' )->Log(
             Priority => 'error',
             Message  => $@,
         );
@@ -207,33 +234,60 @@ sub CleanUp {
     return 1;
 }
 
-sub _Connect {
+
+sub _Connect
+{
     my $Self = shift;
 
     return if $Self->{Redis};
 
-    my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
-    my $Loaded = $MainObject->Require(
-        $Self->{Config}{RedisFast} ? 'Redis::Fast' : 'Redis',
-    );
+    my $MainObject = $Kernel::OM->Get( 'Kernel::System::Main' );
+    my $Loaded     = $MainObject->Require( $Self->{Config}{RedisFast} ? 'Redis::Fast' : 'Redis', );
     return if !$Loaded;
 
+    my $use_socket = 0;
+    if ( -e $Self->{Redis}->{Config}{Socket} )
+    {
+        $use_socket = 1;
+    }
+
     eval {
-        if ( $Self->{Config}{RedisFast} ) {
-            $Self->{Redis} = Redis::Fast->new( server => $Self->{Config}{Address} );
+        if ( $Self->{Config}{RedisFast} )
+        {
+            if ( $use_socket )
+            {
+                $Self->{Redis} =
+                    Redis::Fast->new( socket => $Self->{Config}{Socket} );
+            }
+            else
+            {
+                $Self->{Redis} =
+                    Redis::Fast->new( server => $Self->{Config}{Address} );
+            }
+
         }
-        else {
-            $Self->{Redis} = Redis->new( server => $Self->{Config}{Address} );
+        else
+        {
+            if ( $use_socket )
+            {
+                $Self->{Redis} = Redis->new( socket => $Self->{Config}{Socket} );
+            }
+            else
+            {
+                $Self->{Redis} = Redis->new( server => $Self->{Config}{Address} );
+            }
         }
-        if (
-            $Self->{Config}{DatabaseNumber}
+
+        if ( $Self->{Config}{DatabaseNumber}
             && !$Self->{Redis}->select( $Self->{Config}{DatabaseNumber} ) )
         {
             die "Can't select database '$Self->{Config}{DatabaseNumber}'!";
         }
     };
-    if ($@) {
-        $Kernel::OM->Get('Kernel::System::Log')->Log(
+
+    if ( $@ )
+    {
+        $Kernel::OM->Get( 'Kernel::System::Log' )->Log(
             Priority => 'error',
             Message  => "Redis error: $@!",
         );
